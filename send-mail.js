a87d32812a16f9640561be72f969afad9bc6e2c6
Symul Kabir <saimonpranta@gmail.com>
2025-11-04 11:57:19 +0600

    Update send-mail.js file wich new config

--- send-mail.js
+++ send-mail.js
@@ -1,23 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.somacharnews.com",
-  port: 587, // or 25 if 587 blocked
-  secure: false, // use TLS if true
+  host: "smtp.hostinger.com", // Hostinger SMTP host
+  port: 587,
+  secure: false,
   auth: {
-    user: "symul@somacharnews.com", // your SMTP email
-    pass: "your-email-password",    // your SMTP password
+    user: "symul@somacharnews.com",
+    pass: "your-email-password",
   },
 });
 
 const mailOptions = {
   from: "symul@somacharnews.com",
   to: "saimonpranta@gmail.com",
-  subject: "Test Email from Production",
-  text: "Hello! This is a test email sent from my production server.",
+  subject: "Test Email",
+  text: "Hello! This is a test email from production.",
 };
 
 transporter.sendMail(mailOptions, (err, info) => {
   if (err) console.error("Error sending email:", err);
-  else console.log("Email sent successfully:", info.response);
+  else console.log("Email sent:", info.response);
 });
